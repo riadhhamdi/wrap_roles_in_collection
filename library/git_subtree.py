6d37e75b50929dc18891b8e4cdeaf037a59cd441
automated collection packaging <rhamdi@redhat.com>
2023-05-06 00:53:07 +0200

    Adjusting module git_subtree

--- library/git_subtree.py
+++ library/git_subtree.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Written by Riadh Hamdi <rhamdi@redhat.com> <ryadh.hamdi@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 
 DOCUMENTATION = '''
 module: git_subtree
 short_description: Ansible module that mimic git subtree add/pull in an idempotent way.
 description:
     - This module mimics the functionality of the git subtree command by adding a subtree from a source repository to a subdirectory in the main repository.
-version_added: "2.9"
+version_added: "2.16"
 author:
-    - Riadh Hamdi (@riadhhamdi) (rhamdi@redhat.com) 
+    - Riadh Hamdi (@riadhhamdi) (rhamdi@redhat.com)
 options:
     source:
         description:
             - The source repository to pull the subtree from.
         required: true
+        type: str
     ref:
         description:
             - The repository ref while adding or pulling subtree. Example a branch (main,develop..) of a specific tag
         required: true
+        type: str
     prefix:
         description:
             - The prefix to use for the subtree directory in the main repository.
         required: true
+        type: str
     squash:
         description:
             - A boolean flag indicating whether to squash the subtree history into a single commit in the main repository.
@@ -30,10 +40,12 @@ options:
         description:
             - The commit message to use when committing the subtree changes to the main repository.
         default: ''
+        type: str
     working_directory:
         description:
             - The working directory in which to execute the git command.
         default: null
+        type: str
 '''
 
 EXAMPLES = '''
@@ -45,7 +57,6 @@ EXAMPLES = '''
     squash: true
     commit_message: "Add subtree from example/repo"
     working_directory: /path/to/main/repository
-
 - name: Add/Pull a subtree to the main repository using ssh
   git_subtree:
     source: git@github.com/example/repo.git
@@ -54,9 +65,7 @@ EXAMPLES = '''
     squash: true
     commit_message: "Add subtree from example/repo using ssd"
     working_directory: /path/to/main/repository
-
-
-- name: Add/Pull a subtree to the main repository using http and disabling git password prompt 
+- name: Add/Pull a subtree to the main repository using http and disabling git password prompt
   git_subtree:
     source: https://github.com/example/repo.git
     ref: main
@@ -66,8 +75,7 @@ EXAMPLES = '''
     working_directory: /path/to/main/repository
   environment:
     GIT_TERMINAL_PROMPT: 0
-
-- name: Add/Pull multiple subtrees to the main repository using http and disabling git password prompt 
+- name: Add/Pull multiple subtrees to the main repository using http and disabling git password prompt
   git_subtree:
     source: "{{item.source}}"
     ref: "{{item.ref}}"
@@ -78,12 +86,12 @@ EXAMPLES = '''
   environment:
     GIT_TERMINAL_PROMPT: 0
   vars:
-    roles_repositories: 
+    roles_repositories:
       - source: https://github.com/example/role1.git
         squash: true
         ref: main
         prefix: roles/role1
-      - source: https://github.com/example/role2.git 
+      - source: https://github.com/example/role2.git
         squash: true
         ref: develop
         prefix: roles/role2
@@ -91,8 +99,7 @@ EXAMPLES = '''
         squash: true
         ref: 1.0.4
         prefix: roles/role3
-
-- name: Add/Pull a subtree with authentication (read only token) 
+- name: Add/Pull a subtree with authentication (read only token)
   git_subtree:
     source: https://0auth:ghp_2234xxxxxxxxxx5@github.com/example/repo.git
     ref: main
@@ -102,12 +109,19 @@ EXAMPLES = '''
     working_directory: /path/to/main/repository
   environment:
     GIT_TERMINAL_PROMPT: 0
-
 '''
 
+RETURN = '''
+msg:
+  description: The response body content.
+  returned: on success
+  type: str
+  sample: "{}"
+'''
+
+import os
 from ansible.module_utils.basic import AnsibleModule
-import subprocess
-import os 
+
 
 def main():
     module = AnsibleModule(
@@ -117,10 +131,7 @@ def main():
             prefix=dict(required=True),
             squash=dict(type='bool', default=False),
             commit_message=dict(default=''),
-            working_directory=dict(default=None),
-            username=dict(default=None),
-            password=dict(default=None, no_log=True),
-            ssh_key=dict(default=None)
+            working_directory=dict(default=None)
         ),
         supports_check_mode=False,
     )
@@ -138,42 +149,51 @@ def main():
             command.append('--squash')
         if commit_message:
             command.extend(['-m', commit_message])
-    
         try:
-            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=working_directory, check=True, text=True)
-        except subprocess.CalledProcessError as e:
-            module.fail_json(msg=f"Error running command {e.cmd}: {e.stderr.strip()}", rc=e.returncode)
-    
+            rc, stdout, stderr = module.run_command(command, cwd=working_directory, check_rc=True)
+        except Exception as e:
+            module.fail_json(msg="Error running command %s" % e.cmd, rc=e.returncode)
+        mesg = stdout.strip()
         module.exit_json(
             changed=True,
-            msg=result.stdout.strip(),
-            rc=result.returncode
+            failed=False,
+            msg='Added new subtree %s' % prefix,
+            stdout=mesg,
+            stderr='',
+            rc=rc
         )
-    else:    
+    else:
         # the subtree already exists, update it
         command = ['git', 'subtree', 'pull', '--prefix', prefix, source, ref]
         if squash:
             command.append('--squash')
         if commit_message:
             command.extend(['-m', commit_message])
-    
+
         try:
-            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=working_directory, check=True, text=True)
-        except subprocess.CalledProcessError as e:
-            module.fail_json(msg=f"Error running command {e.cmd}: {e.stderr.strip()}", rc=e.returncode)
-        if 'is already at commit' in result.stderr:
+            rc, stdout, stderr = module.run_command(command, cwd=working_directory, check_rc=True)
+        except Exception as e:
+            module.fail_json(msg="Error running command %s" % e.cmd, rc=e.returncode)
+        if 'is already at commit' in stderr:
+            mesg = stdout.strip()
             module.exit_json(
                 changed=False,
-                msg=result.stdout.strip(),
-                rc=result.returncode
+                failed=False,
+                msg="skipped since already at commit",
+                stdout=mesg,
+                stderr='',
+                rc=rc
             )
         else:
-    
+            mesg = stdout.strip()
             module.exit_json(
                 changed=True,
-                msg=result.stdout.strip(),
-                rc=result.returncode
+                msg='Updated existing subtree %s' % prefix,
+                stdout=mesg,
+                stderr='',
+                rc=rc
             )
+    return True
 
 
 if __name__ == '__main__':
